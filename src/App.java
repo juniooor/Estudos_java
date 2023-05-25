3c755ab9862d8aa94fc2c1168ce81c5373417c16
Juniooor <juniormodern@hotmail.com>
2023-05-24 22:17:28 -0300

    treinando variaveis

--- src/App.java
+++ src/App.java
@@ -1,6 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.printf("Hello, World!");
+        int a;
+        int b = 2;
+        a = 3;
+
+        int soma = a + b;
+        int subtracao = a - b;
+        int mutiplicacao = a * b;
+        float divisao = (float)a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(mutiplicacao);
+        System.out.println(divisao);
+
     }
 }
