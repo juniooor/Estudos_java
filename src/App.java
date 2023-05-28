984d7cdc680d59d66174e22f91dbeddf6bdb7dfa
Juniooor <juniormodern@hotmail.com>
2023-05-28 11:39:23 -0300

    POO em java

--- src/App.java
+++ src/App.java
@@ -1,36 +1,18 @@
-import java.text.NumberFormat.Style;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.TextStyle;
-import java.util.Locale;
-import java.util.Scanner;
+import Animais.Cachorro;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        String nome = "Junior";
-        System.out.println(nome.toUpperCase());
-        String outronome = "JUnior";
-        System.out.println(nome.equalsIgnoreCase(outronome));
+    public static void main(String[] args) {
+        Cachorro cachorro1;
+        cachorro1 = new Cachorro();
 
-        LocalDate today = LocalDate.now();
-        Locale country = new Locale("pt", "BR");
-        String diaDaSemana = today.getDayOfWeek().getDisplayName(TextStyle.FULL, country);
-        System.out.println(diaDaSemana);
-        String saudacao;
-        LocalDateTime agora = LocalDateTime.now();
-        if (agora.getHour() >= 0 && agora.getHour() <12 ){
-            saudacao = "Bom dia! ";
-        } 
-        else if (agora.getHour() >=12 && agora.getHour() <18 ){
-            saudacao = "Boa tarde! ";
-        }
-        else if (agora.getHour() >=18 && agora.getHour() <24 ){
-            saudacao = "Boa noite! ";
-        }
-        else {
-            saudacao = "olá";
-        }
+        cachorro1.nome = "Cacau";
+        cachorro1.cor = "Marron";
+        cachorro1.altura = 40;
+        cachorro1.peso = 25.3;
+        cachorro1.tamanhoDoRabo = 30;
 
-        System.out.printf("Olá %s. , HOje é %s. , %s %n", nome,diaDaSemana, saudacao.toUpperCase());
+
     }   
-}
+
+    
+
